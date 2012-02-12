562e2d79193b4ebba3947bb830573d981f0faaa3
Matthew Steedman <matt@tankjetstudios.com>
2012-02-12 12:06:39 -0500

    removed window settings

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ require('controls')
 
 function love.load()
 	love.graphics.setBackgroundColor(255, 255, 255)
-	love.graphics.setMode(650, 650, false, true, 0) --set the window dimensions to 650 by 650 with no fullscreen, vsync on, and no antialiasing
 	cammy = sprites.new('assets/cammy.png')
 	sprites.new_state(cammy, "stance", 78, 93, 482, {70,160,248,334,421,})
 	sprites.new_state(cammy, "walk_forward", 75, 104, 587, {74,152,231,309,389,468,551,631,708,788})
